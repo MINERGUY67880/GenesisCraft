cde433d7f7e9ca960a6a5b08a4efe121ea2f8c3f
MINERGUY67880 <theminerguy67880@gmail.com>
2016-01-05 15:24:04 -0600

    Started configuration reading

---
+++ GenesisCraft/ConfigGUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConfigGUI
+{
+	public class Program
+	{
+		public Program ()
+		{
+
+		}
+
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Form form = new Form ();
+			form.Text = "ConfigGUI";
+			form.ShowDialog ();
+			TabControl tabControl = new TabControl ();
+			TabPage page1 = new TabPage ("General");
+			tabControl.TabPages.Add (page1);
+			form.Controls.Add (tabControl);
+			tabControl.Show ();
+		}
+
+
+	}
+}
+
---
+++ GenesisCraft/GenesisCraftCLI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace GenesisCraftCLI
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Starting Server...");
+			if (File.Exists ("config.xml")) {
+				Console.WriteLine ("Configuration File Found, Loading...");
+				parseXML ();
+			} else {
+				Console.WriteLine ("Configuration File Missing");
+			}
+		}
+
+		public static void parseXML()
+		{
+			XmlReader xmlReader = XmlReader.Create ("config.xml");
+			while (xmlReader.Read()) 
+			{
+				if((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Setting"))
+				{
+					if(xmlReader.HasAttributes)
+						Console.WriteLine(xmlReader.GetAttribute("playerCap") + ": " + xmlReader.GetAttribute("value"));                    
+				}
+			}
+		}
+	}
+}
